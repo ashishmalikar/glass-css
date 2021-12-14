bdb98a6b3686cedefe45db69cf785af1fdbcabb2
ashishmalikar <ashishmalikar@gmail.com>
2021-12-14 20:41:56 +0530

    New Changes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { dropdown } from "./sass/components/dropdown";
+import { Accordion } from "./js/Accordion";
+
+// export { $Gls.Calendar } from "./sass/components/calendar"
+
+export const $Gls = {
+  dropdown,
+  Accordion
+}
+
